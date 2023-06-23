417eff6982cdbfdccef0814ecc1ff02ed69a0de0
navalona <nnavalona@gmail.com>
2023-06-23 08:46:42 +0300

    fix roure

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -269,131 +269,144 @@ class RouteController extends AbstractController
                  * Administrative data
                  */
                 //Foncier
-                $data['statut'] = $request->get('statutFoncier');
-                $data['numeroReference'] = $request->get('numeroReferenceFoncier');
-                $data['nomProprietaire'] = $request->get('nomProprietaireFoncier');
-
-                $idFoncier = $routeService->addInfrastructureRouteFoncier($idInfra, $data);
-
+                if (null != $request->get('hasFoncier') && ($request->get('hasFoncier') == true || $request->get('hasFoncier') == "true")) {
+                    $data['statut'] = $request->get('statutFoncier');
+                    $data['numeroReference'] = $request->get('numeroReferenceFoncier');
+                    $data['nomProprietaire'] = $request->get('nomProprietaireFoncier');
+    
+                    $idFoncier = $routeService->addInfrastructureRouteFoncier($idInfra, $data);
+    
+                }
+               
                 //Travaux 
-                $data['objetTravaux'] = $request->get('objetTravaux');
-                $data['consistanceTravaux'] = $request->get('consistanceTravaux');
-                $data['modeRealisationTravaux'] = $request->get('modeRealisationTravaux');
-                $data['maitreOuvrageTravaux'] = $request->get('maitreOuvrageTravaux');
-                $data['maitreOuvrageDelegueTravaux'] = $request->get('maitreOuvrageDelegueTravaux');
-                $data['idControleSurveillanceTravaux'] = $request->get('idControleSurveillanceTravaux');//idControleSurveillance
-                $data['modePassationTravaux'] = null;
+                if (null != $request->get('hasTravaux') && ($request->get('hasTravaux') == true || $request->get('hasTravaux') == "true")) {
 
-                if (null != $request->get('modePassationTravaux')) {
-                    $data['modePassationTravaux'] = $request->get('modePassationTravaux');
+                    $data['objetTravaux'] = $request->get('objetTravaux');
+                    $data['consistanceTravaux'] = $request->get('consistanceTravaux');
+                    $data['modeRealisationTravaux'] = $request->get('modeRealisationTravaux');
+                    $data['maitreOuvrageTravaux'] = $request->get('maitreOuvrageTravaux');
+                    $data['maitreOuvrageDelegueTravaux'] = $request->get('maitreOuvrageDelegueTravaux');
+                    $data['idControleSurveillanceTravaux'] = $request->get('idControleSurveillanceTravaux');//idControleSurveillance
+                    $data['modePassationTravaux'] = null;
+
+                    if (null != $request->get('modePassationTravaux')) {
+                        $data['modePassationTravaux'] = $request->get('modePassationTravaux');
+                    }
+                    
+                    $data['porteAppelOffreTravaux'] = $request->get('porteAppelOffreTravaux');
+                    $data['montantTravaux'] = $request->get('montantTravaux');
+                    $data['numeroContratTravaux'] = $request->get('numeroContratTravaux');
+                    $data['precisionConsistanceTravaux'] = $request->get('precisionConsistanceTravaux');
+                    
+                    $dateContratTravaux = new \DateTime($request->get('dateContratTravaux'));
+                    $dateContratTravaux->format('Y-m-d H:i:s');
+                    $data['dateContratTravaux'] = $dateContratTravaux;
+
+                    $dateOrdreServiceTravaux = new \DateTime($request->get('dateOrdreServiceTravaux'));
+                    $dateOrdreServiceTravaux->format('Y-m-d H:i:s');
+
+                    $data['dateOrdreServiceTravaux'] = $dateOrdreServiceTravaux;
+                    $data['idTitulaireTravaux'] = $request->get('idTitulaireTravaux');//idTitulaire
+                    $data['resultatTravaux'] = $request->get('resultatTravaux');
+                    $data['motifRuptureContratTravaux'] = $request->get('motifRuptureContratTravaux');
+                    $dateReceptionProvisoireTravaux = new \DateTime($request->get('dateReceptionProvisoireTravaux'));
+                    $dateReceptionProvisoireTravaux->format('Y-m-d H:i:s');
+                    $data['dateReceptionProvisoireTravaux'] = $dateReceptionProvisoireTravaux;
+                    $dateReceptionDefinitiveTravaux = new \DateTime($request->get('dateReceptionDefinitiveTravaux'));
+                    $dateReceptionDefinitiveTravaux->format('Y-m-d H:i:s');
+                    $data['dateReceptionDefinitiveTravaux'] = $dateReceptionDefinitiveTravaux;
+                    $data['ingenieurReceptionProvisoireTravaux'] = $request->get('ingenieurReceptionProvisoireTravaux');
+                    $data['ingenieurReceptionDefinitiveTravaux'] = $request->get('ingenieurReceptionDefinitiveTravaux');
+                    $data['dateInformationTravaux'] = new \DateTime();
+                    $data['sourceInformationTravaux'] = $request->get('sourceInformationTravaux');
+                    $data['modeAcquisitionInformationTravaux'] = $request->get('modeAcquisitionInformationTravaux');
+                    $data['bailleurTravaux'] = $request->get('bailleurTravaux');
+
+                    $idTravaux = $routeService->addInfrastructureRouteTravaux($idInfra, $data);
                 }
                 
-                $data['porteAppelOffreTravaux'] = $request->get('porteAppelOffreTravaux');
-                $data['montantTravaux'] = $request->get('montantTravaux');
-                $data['numeroContratTravaux'] = $request->get('numeroContratTravaux');
-                $data['precisionConsistanceTravaux'] = $request->get('precisionConsistanceTravaux');
-                
-                $dateContratTravaux = new \DateTime($request->get('dateContratTravaux'));
-                $dateContratTravaux->format('Y-m-d H:i:s');
-                $data['dateContratTravaux'] = $dateContratTravaux;
-
-                $dateOrdreServiceTravaux = new \DateTime($request->get('dateOrdreServiceTravaux'));
-                $dateOrdreServiceTravaux->format('Y-m-d H:i:s');
-
-                $data['dateOrdreServiceTravaux'] = $dateOrdreServiceTravaux;
-                $data['idTitulaireTravaux'] = $request->get('idTitulaireTravaux');//idTitulaire
-                $data['resultatTravaux'] = $request->get('resultatTravaux');
-                $data['motifRuptureContratTravaux'] = $request->get('motifRuptureContratTravaux');
-                $dateReceptionProvisoireTravaux = new \DateTime($request->get('dateReceptionProvisoireTravaux'));
-                $dateReceptionProvisoireTravaux->format('Y-m-d H:i:s');
-                $data['dateReceptionProvisoireTravaux'] = $dateReceptionProvisoireTravaux;
-                $dateReceptionDefinitiveTravaux = new \DateTime($request->get('dateReceptionDefinitiveTravaux'));
-                $dateReceptionDefinitiveTravaux->format('Y-m-d H:i:s');
-                $data['dateReceptionDefinitiveTravaux'] = $dateReceptionDefinitiveTravaux;
-                $data['ingenieurReceptionProvisoireTravaux'] = $request->get('ingenieurReceptionProvisoireTravaux');
-                $data['ingenieurReceptionDefinitiveTravaux'] = $request->get('ingenieurReceptionDefinitiveTravaux');
-                $data['dateInformationTravaux'] = new \DateTime();
-                $data['sourceInformationTravaux'] = $request->get('sourceInformationTravaux');
-                $data['modeAcquisitionInformationTravaux'] = $request->get('modeAcquisitionInformationTravaux');
-                $data['bailleurTravaux'] = $request->get('bailleurTravaux');
-
-                $idTravaux = $routeService->addInfrastructureRouteTravaux($idInfra, $data);
                 // Fournitures
-                $data['objetContratFourniture'] = $request->get('objetContratFourniture');
-                $data['consistanceContratFourniture'] = $request->get('consistanceContratFourniture');
-                $data['materielsFourniture'] = $request->get('materielsFourniture');
-                $data['entiteFourniture'] = $request->get('entiteFourniture');
-                $data['modePassationFourniture'] = $request->get('modePassationFourniture');
-                $data['porteAppelOffreFourniture'] = $request->get('porteAppelOffreFourniture');
-                $data['montantFourniture'] = $request->get('montantFourniture');
-                $data['idTitulaireFourniture'] = $request->get('idTitulaireFourniture');
-                $data['numeroContratFourniture'] = $request->get('numeroContratFourniture');
-                $dateContratFourniture = new \DateTime($request->get('dateContratFourniture'));
-                $dateContratFourniture->format('Y-m-d H:i:s');
+                if (null != $request->get('hasFourniture') && ($request->get('hasFourniture') == true || $request->get('hasFourniture') == "true")) {
+                    $data['objetContratFourniture'] = $request->get('objetContratFourniture');
+                    $data['consistanceContratFourniture'] = $request->get('consistanceContratFourniture');
+                    $data['materielsFourniture'] = $request->get('materielsFourniture');
+                    $data['entiteFourniture'] = $request->get('entiteFourniture');
+                    $data['modePassationFourniture'] = $request->get('modePassationFourniture');
+                    $data['porteAppelOffreFourniture'] = $request->get('porteAppelOffreFourniture');
+                    $data['montantFourniture'] = $request->get('montantFourniture');
+                    $data['idTitulaireFourniture'] = $request->get('idTitulaireFourniture');
+                    $data['numeroContratFourniture'] = $request->get('numeroContratFourniture');
+                    $dateContratFourniture = new \DateTime($request->get('dateContratFourniture'));
+                    $dateContratFourniture->format('Y-m-d H:i:s');
 
-                $data['dateContratFourniture'] = $dateContratFourniture;
+                    $data['dateContratFourniture'] = $dateContratFourniture;
 
-                $dateOrdreFourniture = new \DateTime($request->get('dateOrdreFourniture'));
-                $dateOrdreFourniture->format('Y-m-d H:i:s');
+                    $dateOrdreFourniture = new \DateTime($request->get('dateOrdreFourniture'));
+                    $dateOrdreFourniture->format('Y-m-d H:i:s');
 
-                $data['dateOrdreFourniture'] = $dateOrdreFourniture;
-                $data['resultatFourniture'] = $request->get('resultatFourniture');
-                $data['raisonResiliationFourniture'] = $request->get('raisonResiliationFourniture');
-                $data['ingenieurReceptionProvisoireFourniture'] = $request->get('ingenieurReceptionProvisoireFourniture');
-                $data['ingenieurReceptionDefinitiveFourniture'] = $request->get('ingenieurReceptionDefinitiveFourniture');
-                
-                $dateReceptionProvisoireFourniture = new \DateTime($request->get('dateReceptionProvisoireFourniture'));
-                $dateReceptionProvisoireFourniture->format('Y-m-d H:i:s');
+                    $data['dateOrdreFourniture'] = $dateOrdreFourniture;
+                    $data['resultatFourniture'] = $request->get('resultatFourniture');
+                    $data['raisonResiliationFourniture'] = $request->get('raisonResiliationFourniture');
+                    $data['ingenieurReceptionProvisoireFourniture'] = $request->get('ingenieurReceptionProvisoireFourniture');
+                    $data['ingenieurReceptionDefinitiveFourniture'] = $request->get('ingenieurReceptionDefinitiveFourniture');
+                    
+                    $dateReceptionProvisoireFourniture = new \DateTime($request->get('dateReceptionProvisoireFourniture'));
+                    $dateReceptionProvisoireFourniture->format('Y-m-d H:i:s');
 
-                $data['dateReceptionProvisoireFourniture'] = $dateReceptionProvisoireFourniture;
-                
-                $dateReceptionDefinitiveFourniture = new \DateTime($request->get('dateReceptionDefinitiveFourniture'));
-                $dateReceptionDefinitiveFourniture->format('Y-m-d H:i:s');
+                    $data['dateReceptionProvisoireFourniture'] = $dateReceptionProvisoireFourniture;
+                    
+                    $dateReceptionDefinitiveFourniture = new \DateTime($request->get('dateReceptionDefinitiveFourniture'));
+                    $dateReceptionDefinitiveFourniture->format('Y-m-d H:i:s');
 
-                $data['dateReceptionDefinitiveFourniture'] = $dateReceptionDefinitiveFourniture;
-                $data['bailleurFourniture'] = $request->get('bailleurFourniture');
-                $idFourniture = $routeService->addInfrastructureRouteFourniture($idInfra, $data);
-                // Etudes
-                $data['objetContratEtude'] = $request->get('objetContratEtude');
-                $data['consistanceContratEtude'] = $request->get('consistanceContratEtude');
-                $data['entiteEtude'] = $request->get('entiteEtude');
-                $data['idTitulaireEtude'] = $request->get('idTitulaireEtude');
-                $data['montantContratEtude'] = $request->get('montantContratEtude');
-                $data['numeroContratEtude'] = null ;
-                if (null != $request->get('numeroContratEtude')) {
-                    $data['numeroContratEtude'] = $request->get('numeroContratEtude');
+                    $data['dateReceptionDefinitiveFourniture'] = $dateReceptionDefinitiveFourniture;
+                    $data['bailleurFourniture'] = $request->get('bailleurFourniture');
+                    $idFourniture = $routeService->addInfrastructureRouteFourniture($idInfra, $data);
                 }
                 
-                $data['modePassationEtude'] = $request->get('modePassationEtude');
-                $data['porteAppelOffreEtude'] = $request->get('porteAppelOffreEtude');
+                // Etudes
+                if (null != $request->get('hasEtude') && ($request->get('hasEtude') == true || $request->get('hasEtude') == "true")) {
+                    $data['objetContratEtude'] = $request->get('objetContratEtude');
+                    $data['consistanceContratEtude'] = $request->get('consistanceContratEtude');
+                    $data['entiteEtude'] = $request->get('entiteEtude');
+                    $data['idTitulaireEtude'] = $request->get('idTitulaireEtude');
+                    $data['montantContratEtude'] = $request->get('montantContratEtude');
+                    $data['numeroContratEtude'] = null ;
+                    if (null != $request->get('numeroContratEtude')) {
+                        $data['numeroContratEtude'] = $request->get('numeroContratEtude');
+                    }
+                    
+                    $data['modePassationEtude'] = $request->get('modePassationEtude');
+                    $data['porteAppelOffreEtude'] = $request->get('porteAppelOffreEtude');
 
-                $dateContratEtude = new \DateTime($request->get('dateContratEtude'));
-                $dateContratEtude->format('Y-m-d H:i:s');
+                    $dateContratEtude = new \DateTime($request->get('dateContratEtude'));
+                    $dateContratEtude->format('Y-m-d H:i:s');
 
-                $data['dateContratEtude'] = $dateContratEtude;
+                    $data['dateContratEtude'] = $dateContratEtude;
 
-                $dateOrdreServiceEtude = new \DateTime($request->get('dateOrdreServiceEtude'));
-                $dateOrdreServiceEtude->format('Y-m-d H:i:s');
+                    $dateOrdreServiceEtude = new \DateTime($request->get('dateOrdreServiceEtude'));
+                    $dateOrdreServiceEtude->format('Y-m-d H:i:s');
 
-                $data['dateOrdreServiceEtude'] = $dateOrdreServiceEtude;
-                $data['resultatPrestationEtude'] = $request->get('resultatPrestationEtude');
-                /*if (null != $request->get('resultatPrestationEtude') && strlen($request->get('resultatPrestationEtude')) <= 20) {
+                    $data['dateOrdreServiceEtude'] = $dateOrdreServiceEtude;
                     $data['resultatPrestationEtude'] = $request->get('resultatPrestationEtude');
-                } else {
-                   throw new \Exception("Resultat prestation etude doit etre une chaine au maximal 20 caractere");
-                }*/
+                    /*if (null != $request->get('resultatPrestationEtude') && strlen($request->get('resultatPrestationEtude')) <= 20) {
+                        $data['resultatPrestationEtude'] = $request->get('resultatPrestationEtude');
+                    } else {
+                    throw new \Exception("Resultat prestation etude doit etre une chaine au maximal 20 caractere");
+                    }*/
+                    
+                    $data['motifRuptureContratEtude'] = $request->get('motifRuptureContratEtude');
+                    
+                    $dateInformationEtude = new \DateTime($request->get('dateInformationEtude'));
+                    $dateInformationEtude->format('Y-m-d H:i:s');
+                    
+                    $data['dateInformationEtude'] = $dateInformationEtude;
+                    $data['sourceInformationEtude'] = $request->get('sourceInformationEtude');
+                    $data['modeAcquisitionInformationEtude'] = $request->get('modeAcquisitionInformationEtude');
+                    $data['precisionConsistanceContratEtude'] = $request->get('precisionConsistanceContratEtude');
+                    $data['bailleurEtude'] = $request->get('bailleurEtude');
+                    $idEtude = $routeService->addInfrastructureRouteEtudes($idInfra, $data);
+                }
                 
-                $data['motifRuptureContratEtude'] = $request->get('motifRuptureContratEtude');
-                
-                $dateInformationEtude = new \DateTime($request->get('dateInformationEtude'));
-                $dateInformationEtude->format('Y-m-d H:i:s');
-                
-                $data['dateInformationEtude'] = $dateInformationEtude;
-                $data['sourceInformationEtude'] = $request->get('sourceInformationEtude');
-                $data['modeAcquisitionInformationEtude'] = $request->get('modeAcquisitionInformationEtude');
-                $data['precisionConsistanceContratEtude'] = $request->get('precisionConsistanceContratEtude');
-                $data['bailleurEtude'] = $request->get('bailleurEtude');
-                $idEtude = $routeService->addInfrastructureRouteEtudes($idInfra, $data);
                 /**
                  * End Administrative data
                 */

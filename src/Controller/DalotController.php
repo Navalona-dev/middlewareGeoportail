061526c1287fecc31e92f650ac72049590d2cc43
navalona <nnavalona@gmail.com>
2023-07-01 11:43:58 +0300

    fix bgug

--- src/Controller/DalotController.php
+++ src/Controller/DalotController.php
@@ -161,12 +161,13 @@ class DalotController extends AbstractController
                 $tmpPathName1 = $uploadedFile1->getPathname();
                 $directory1 = $this->pathImageDalot . "photo1/";
                 $directoryPublic = $this->kernelInterface->getProjectDir().$this->pathPublic . "dalot/photo1/";
+                $directoryPublicCopy = $this->kernelInterface->getProjectDir()."public".$this->pathPublic . "dalot/photo1/";
 
                 $name_temp = hash('sha512', session_id().microtime($nomOriginal1));
                 $nomPhoto1 = $name_temp.".".$uploadedFile1->getClientOriginalExtension();
                 
                 move_uploaded_file($tmpPathName1, $directory1.$nomPhoto1);
-                copy($directory1.$nomPhoto1, $directoryPublic.$nomPhoto1);
+                copy($directory1.$nomPhoto1, $directoryPublicCopy.$nomPhoto1);
                 //move_uploaded_file($tmpPathName1, $directoryPublic.$nomPhoto1);
 
                 $data['photo1'] = $this->pathForNamePhotoDalot."photo1/" .$nomPhoto1;
@@ -178,11 +179,12 @@ class DalotController extends AbstractController
                 $tmpPathName2 = $uploadedFile2->getPathname();
                 $directory2 = $this->pathImageDalot . "photo2/";
                 $directoryPublic = $this->kernelInterface->getProjectDir().$this->pathPublic . "dalot/photo2/";
+                $directoryPublicCopy = $this->kernelInterface->getProjectDir()."public".$this->pathPublic . "dalot/photo2/";
 
                 $name_temp2 = hash('sha512', session_id().microtime($nomOriginal2));
                 $nomPhoto2 = $name_temp2.".".$uploadedFile2->getClientOriginalExtension();
                 move_uploaded_file($tmpPathName2, $directory2.$nomPhoto2);
-                copy($directory2.$nomPhoto2, $directoryPublic.$nomPhoto2);
+                copy($directory2.$nomPhoto2, $directoryPublicCopy.$nomPhoto2);
                 //move_uploaded_file($tmpPathName2, $directoryPublic.$nomPhoto2);
                 
                 $data['photo2'] = $this->pathForNamePhotoDalot."photo2/" .$nomPhoto2;
@@ -194,11 +196,12 @@ class DalotController extends AbstractController
                 $tmpPathName3 = $uploadedFile3->getPathname();
                 $directory3 = $this->pathImageDalot . "photo3/";
                 $directoryPublic = $this->kernelInterface->getProjectDir().$this->pathPublic . "dalot/photo3/";
+                $directoryPublicCopy = $this->kernelInterface->getProjectDir()."public".$this->pathPublic . "dalot/photo3/";
 
                 $name_temp3 = hash('sha512', session_id().microtime($nomOriginal3));
                 $nomPhoto3 = $name_temp3.".".$uploadedFile2->getClientOriginalExtension();
                 move_uploaded_file($tmpPathName3, $directory3.$nomPhoto3);
-                copy($directory3.$nomPhoto3, $directoryPublic.$nomPhoto3);
+                copy($directory3.$nomPhoto3, $directoryPublicCopy.$nomPhoto3);
                 //move_uploaded_file($tmpPathName3, $directoryPublic.$nomPhoto3);
 
                 $data['photo3'] = $this->pathForNamePhotoDalot."photo3/" .$nomPhoto3;

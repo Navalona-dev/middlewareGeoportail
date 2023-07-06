dc7ab3c59924a037e6fe3283b3e0c1f8beeef25a
navalona <nnavalona@gmail.com>
2023-07-06 11:52:26 +0300

    fix correctif

--- src/Service/TrajetrouteService.php
+++ src/Service/TrajetrouteService.php
@@ -195,7 +195,7 @@ class TrajetrouteService
 
     public function addInfrastructureRouteFourniture($idInfrastructure = null, $data)
     {
-        $result = $this->trajetrouteRepository->addInfrastructureRouteFourniture($data['objetContratFourniture'], $data['materielsFouriniture'], $data['entiteFourniture'], $data['modePassationFourniture'], $data['porteAppelOffreFourniture'], $data['montantFourniture'], $data['idTitulaireFourniture'], $data['numeroContratFourniture'], $data['dateContratFourniture'], $data['dateOrdreFourniture'], $data['resultatFourniture'], $data['raisonResiliationFourniture'], $idInfrastructure, $data['bailleurFourniture'], $data['consistanceContratFourniture'], $data['precisionPassationFourniture']);
+        $result = $this->trajetrouteRepository->addInfrastructureRouteFourniture($data['objetContratFourniture'], $data['materielsFouriniture'], $data['entiteFourniture'], $data['modePassationFourniture'], $data['porteAppelOffreFourniture'], $data['montantFourniture'], $data['idTitulaireFourniture'], $data['numeroContratFourniture'], $data['dateContratFourniture'], $data['dateOrdreFourniture'], $data['resultatFourniture'], $data['raisonResiliationFourniture'], $data['dateReceptionProvisoireFourniture'], $data['dateReceptionDefinitiveFourniture'], $data['ingenieurReceptionProvisoireFourniture'], $data['ingenieurReceptionDefinitiveFourniture'], $idInfrastructure, $data['bailleurFourniture'], $data['consistanceContratFourniture'], $data['precisionPassationFourniture']);
         
         if ($result) {
             return $result;

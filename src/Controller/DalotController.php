72824b52b46acb4c63646e40033324bc8a5c094a
navalona <nnavalona@gmail.com>
2024-05-20 17:31:26 +0300

    fix bug dalot rattache

--- src/Controller/DalotController.php
+++ src/Controller/DalotController.php
@@ -1068,7 +1068,7 @@ class DalotController extends AbstractController
                         } elseif(in_array($colonne, $colonneFloat)) {  
                             $value = floatval($value);
                         } else {
-                            if ($colonne == "nom_de_la_route_a_qui_il_est_rattache") {
+                            if ($colonne == "nom") {
                                 if ($value != "null" && $value != "undefined" && $value != "") {
                                     $infoYlisteRoute = $dalotService->getInfoyRouteInfoMinifie($value);
                                     if ($infoYlisteRoute != false && count($infoYlisteRoute) > 0) {

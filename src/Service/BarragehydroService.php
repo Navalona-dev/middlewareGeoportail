dab189c700564007b03f2b20dc7c7efd7c31ebd1
navalona <nnavalona@gmail.com>
2024-08-20 21:32:03 +0300

    fix buff

---
+++ src/Service/BarragehydroService.php
@@ -0,0 +1,255 @@
+<?php
+namespace App\Service;
+
+use App\Service\AuthorizationManager;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Exception\PropertyVideException;
+use App\Exception\ActionInvalideException;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManager;
+use App\Repository;
+use App\Repository\BarragehydroRepository;
+
+class BarragehydroService
+{
+    private $tokenStorage;
+    private $entityManager;
+    private $session;
+    private $barragehydroRepository;
+
+    public function __construct(TokenStorageInterface  $TokenStorageInterface, EntityManager $entityManager, SessionInterface $session, BarragehydroRepository $barragehydroRepository)
+    {
+        $this->tokenStorage = $TokenStorageInterface;
+        $this->entityManager = $entityManager;
+        $this->session = $session;
+        $this->barragehydroRepository = $barragehydroRepository;
+    }
+    
+    public function addInfrastructure($data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructure($data['nom'], $data['categorie'], $data['indicatif'], $data['type'], $data['longueurBarrage'], $data['superficieDominee'], $data['localite'], $data['communeTerrain'], $data['sourceInformation'], $data['modeAcquisitionInformation'], $data['longitude'], $data['latitude'], $data['district'], $data['precisionTpe'], $data['region'], $data['photo1'], $data['photo2'], $data['photo3'], $data['photoName1'], $data['photoName2'], $data['photoName3']);
+        return $result;
+    }
+
+    public function getAllCategorieInfra()
+    {
+        $categories = $this->barragehydroRepository->getAllCategorieInfra();
+        if (count($categories) > 0) {
+            $tabCategorie = [];
+            foreach ($categories as $key => $categorie) {
+                if (!in_array($categorie['categorie'], $tabCategorie)) {
+                    array_push($tabCategorie, $categorie['categorie']);
+                }
+            }
+            return $tabCategorie;
+        }
+        return false;
+    }
+
+    public function getAllInfrastructures()
+    {
+        $routes = $this->barragehydroRepository->getAllInfrastructures();
+        if (count($routes) > 0) {
+            return $routes;
+        }
+        return 0;
+    }
+
+    public function getAllInfrastructuresMinifie()
+    {
+        $routes = $this->barragehydroRepository->getAllInfrastructuresMinifie();
+        if (count($routes) > 0) {
+            return $routes;
+        }
+        return 0;
+    }
+
+    public function updateInfrastructure($idInfra, $updateColonneInfra)
+    {
+        $result = $this->barragehydroRepository->updateInfrastructure($idInfra, $updateColonneInfra);
+        return $result;
+    }
+
+    public function addInfoInTableByInfrastructure($table, $colonnes, $values)
+    {
+        $result = $this->barragehydroRepository->addInfoInTableByInfrastructure($table, $colonnes, $values);
+        return $result;
+    }
+
+    public function updateInfrastructureTables($table , $idRow, $updateColonne)
+    {
+        $result = $this->barragehydroRepository->updateInfrastructureTables($table, $idRow, $updateColonne);
+        return $result;
+    }
+    
+    public function getOneInfraInfo($infraId)
+    {
+        $routes = $this->barragehydroRepository->getOneInfraInfo(intval($infraId));
+        if (count($routes) > 0) {
+            return $routes;
+        }
+        return 0;
+    }
+
+    public function getAllInfrastructuresBaseRoute()
+    {
+        $routes = $this->barragehydroRepository->getAllInfrastructuresBaseRoute();
+        if (count($routes) > 0) {
+            return $routes;
+        }
+        return 0;
+    }
+    
+    public function addInfrastructureRouteEtat($idInfrastructure, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureRouteEtat($idInfrastructure, $data['etat'], $data['sourceInformation'], $data['modeAcquisitionInformation']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+    
+    public function addInfrastructureSituation($idInfrastructure, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureSituation($idInfrastructure, $data['fonctionnel'], $data['motif'], $data['sourceInformationSituation'], $data['modeAcquisitionInformationSituation'], $data['etat'], $data['raisonPrecision']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function addInfrastructureDonneCollecte($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureDonneCollecte($idInfrastructure, $data['betonExistenceFissureBeton'], $data['betonExistenceFerraillageVisible'], $data['betonExistenceAffoullementPiedOuvrage'], $data['betonExistenceErosionLongRivesBarrage'], $data['betonAncrageOuvrageRivesCoursEau'], $data['remblaiFissurationDesRemblais'], $data['remblaiRavinement'], $data['remblaiErosionLongRechargeAval'], $data['remblaiEffetBatillageParementAmont'], $data['remblaiTassement'], $data['degradationParois'], $data['degradationFond'], $data['ensablement'], $data['superficieIrriguee'], $data['sourceInformationData'], $data['modeAcquisitionInformationData']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function addInfrastructureRouteStructure($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureRouteStructure($idInfrastructure, $data['structureDeformation'], $data['structureFissure'], $data['structureFaiencage'], $data['structureNidPouleStructure'], $data['structureDeformation'], $data['structureTeteOndule'], $data['structureRavines'], $data['sourceInformation'], $data['modeAcquisitionInformation']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+    
+    public function addInfrastructureRouteAccotement($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureRouteAccotement($idInfrastructure, $data['accotement'], $data['accotementDegrationSurface'], $data['accotementDentelleRive'], $data['accotementDenivellationChausseAccotement'], $data['accotementDestructionAffouillementAccotement'], $data['accotementNonRevetueDeformationProfil'], $data['accotementRevetue'], $data['accotementTypeRevetementAccotement'], $data['accotementPrecisionTypeAccotement'], $data['dateInformationAccotement'], $data['sourceInformationAccotement'], $data['modeAcquisitionInformationAccotement']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function addInfrastructureRouteFosse($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureRouteFosse($idInfrastructure, $data['coteFosse'], $data['fosseRevetuDegradationFosse'], $data['fosseRevetuSectionBouche'], $data['fosseNonRevetuFosseProfil'], $data['fosseNonRevetuEncombrement'], $data['fosseRevetu'], $data['dateInformationFosse'], $data['sourceInformationFosse'], $data['modeAcquisitionInformationFosse']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function addInfrastructureRouteFoncier($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureRouteFoncier($data['statut'], $data['numeroReference'], $data['nomProprietaire'], $idInfrastructure);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function addInfrastructureTravaux($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureTravaux($idInfrastructure, $data['objetTravaux'], $data['consistanceTravaux'], $data['maitreOuvrageTravaux'], $data['maitreOuvrageDelegueTravaux'], $data['maitreOeuvreTravaux'], $data['idControleSurveillanceTravaux'], $data['modePassationTravaux'], $data['porteAppelOffreTravaux'], $data['montantTravaux'], $data['numeroContratTravaux'], $data['dateContratTravaux'], $data['dateOrdreServiceTravaux'], $data['idTitulaireTravaux'], $data['resultatTravaux'], $data['motifRuptureContratTravaux'], $data['dateReceptionProvisoireTravaux'], $data['dateReceptionDefinitiveTravaux'], $data['ingenieurReceptionProvisoireTravaux'], $data['ingenieurReceptionDefinitiveTravaux'], $data['dateInformationTravaux'], $data['sourceInformationTravaux'], $data['modeAcquisitionInformationTravaux'], $data['bailleurTravaux']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function addInfrastructurePhoto($idInfrastructure = null, $setUpdate)
+    {
+        $result = $this->barragehydroRepository->addInfrastructurePhoto($idInfrastructure, $setUpdate);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function getPhotoInfraInfo($infraId)
+    {
+        $routes = $this->barragehydroRepository->getPhotoInfraInfo(intval($infraId));
+        if (count($routes) > 0) {
+            return $routes;
+        }
+        return false;
+    }
+
+    public function addInfrastructureRouteFourniture($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureRouteFourniture($data['objetContratFourniture'], $data['consistanceContratFourniture'], $data['materielsFourniture'], $data['entiteFourniture'], $data['modePassationFourniture'], $data['porteAppelOffreFourniture'], $data['montantFourniture'], $data['idTitulaireFourniture'], $data['numeroContratFourniture'], $data['dateContratFourniture'], $data['dateOrdreFourniture'], $data['resultatFourniture'], $data['raisonResiliationFourniture'], $data['ingenieurReceptionProvisoireFourniture'], $data['ingenieurReceptionDefinitiveFourniture'], $data['dateReceptionProvisoireFourniture'], $data['dateReceptionDefinitiveFourniture'], $idInfrastructure, $data['bailleurFourniture']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function addInfrastructureEtudes($idInfrastructure = null, $data)
+    {
+        $result = $this->barragehydroRepository->addInfrastructureEtudes($idInfrastructure, $data['objetContratEtude'], $data['consistanceContratEtude'], $data['entiteEtude'], $data['idTitulaireEtude'], $data['montantContratEtude'], $data['numeroContratEtude'], $data['modePassationEtude'], $data['porteAppelOffreEtude'], $data['dateContratEtude'], $data['dateOrdreServiceEtude'], $data['resultatPrestationEtude'], $data['motifRuptureContratEtude'], $data['dateInformationEtude'], $data['sourceInformationEtude'], $data['modeAcquisitionInformationEtude'], $data['bailleurEtude']);
+        
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function cleanTablesByIdInfrastructure($idInfrastructure = null, $type = null)
+    {
+        $this->barragehydroRepository->cleanTablesByIdInfrastructure($idInfrastructure, $type);
+    }
+
+    public function getAllyRouteInfo()
+    {
+        $routeyInfo = $this->barragehydroRepository->getAllyRouteInfo();
+        if (count($routeyInfo) > 0) {
+            return $routeyInfo;
+        }
+        return false;
+    }
+
+    public function update()
+    {
+        $this->entityManager->flush();
+    }
+
+}

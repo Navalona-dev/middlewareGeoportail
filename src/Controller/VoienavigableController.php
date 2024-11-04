69635d7aa225737edfd71f55d0f29154c52aa408
navalona <nnavalona@gmail.com>
2024-11-04 21:30:28 +0300

    fix bug

--- src/Controller/VoienavigableController.php
+++ src/Controller/VoienavigableController.php
@@ -1457,12 +1457,14 @@ class VoienavigableController extends AbstractController
                     }
 
                     if ($valuesInsert) {
-                        /*if ($idSituation == 0 && !$hasDateInformationSituation) {
+                        if ($idSituation == 0 && !$hasDateInformationSituation) {
                             $date = new \DateTime();
                             $dateInfo = $date->format('Y-m-d H:i:s');
-                            $colonneInsert .= "date_information";
+                            $colonneInsert .= ", date_information";
                             $valuesInsert .= "'$dateInfo'";
-                        }*/
+                        }
+                        
+
                         $valuesInsert = trim($valuesInsert);
                         if ($valuesInsert[-1] && $valuesInsert[-1] == ",") {
                             $valuesInsert = substr($valuesInsert, 0, strlen($valuesInsert) - 1);
@@ -1532,12 +1534,12 @@ class VoienavigableController extends AbstractController
                     }
 
                     if ($valuesInsert) {
-                        /*if ($idData == 0 && $hasDateInformationData) {
+                        if ($idData == 0 && $hasDateInformationData) {
                             $date = new \DateTime();
                             $dateInfo = $date->format('Y-m-d H:i:s');
-                            $colonneInsert .= "date_information";
+                            $colonneInsert .= ", date_information";
                             $valuesInsert .= "'$dateInfo'";
-                        }*/
+                        }
                         $valuesInsert = trim($valuesInsert);
                         if ($valuesInsert[-1] && $valuesInsert[-1] == ",") {
                             $valuesInsert = substr($valuesInsert, 0, strlen($valuesInsert) - 1);
